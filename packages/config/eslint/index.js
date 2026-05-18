292d7ead5852f2289bd0e2c5e136c5486014b222
hanif-faiq <haniffaiq95@gmail.com>
2026-05-18 15:42:41 +0700

    feat(config): add shared workspace config package
    Adds @jakartabc/config plus the minimal workspace metadata needed for Phase 0 config consumers.

---
+++ packages/config/eslint/index.js
@@ -0,0 +1,42 @@
+// packages/config/eslint/index.js
+const tseslint = require('@typescript-eslint/eslint-plugin')
+const tsparser = require('@typescript-eslint/parser')
+const reactPlugin = require('eslint-plugin-react')
+const reactHooks = require('eslint-plugin-react-hooks')
+const a11y = require('eslint-plugin-jsx-a11y')
+const prettier = require('eslint-config-prettier')
+
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      'jsx-a11y': a11y,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...a11y.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    settings: { react: { version: 'detect' } },
+  },
+  prettier,
+  {
+    ignores: ['**/dist/**', '**/.next/**', '**/.turbo/**', '**/node_modules/**'],
+  },
+]
